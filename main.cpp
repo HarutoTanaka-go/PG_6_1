9515c9c50da77177fb9367335fbe767a2d36fd25
HarutoTanaka-go <k024g0048@g.neec.ac.jp>
2026-01-19 10:30:46 +0900

    プロジェクト ファイルを追加します。

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <thread>
+
+using namespace std;
+
+void PrintThread(uint32_t num) {
+	cout << "thread" << num << endl;
+}
+
+
+//メイン
+int main() {
+
+	// マルチスレッドではある
+	thread t1(PrintThread, 1);
+	/*スレッドt1開始*/
+	t1.join();
+
+	thread t2(PrintThread, 2);
+	/*スレッドt2開始*/
+	t2.join();
+
+	thread t3(PrintThread, 3);
+	/*スレッドt3開始*/
+	t3.join();
+
+	return 0;
+}
